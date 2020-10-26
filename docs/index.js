2217b22d813728fbabab1f516c2c708252194d36
Daishikofy <daishi.setsuko@gmail.com>
2020-10-26 12:03:20 -0300

    Change web3 version

--- docs/index.js
+++ docs/index.js
@@ -176,7 +176,7 @@ window.onload = async function () {
 
 const ethEnabled = () => {
     if (window.ethereum) {
-        window.web3 = new Web3(window.ethereum);
+        window.web3 = new Web3(window.web3.currentProvider);
         window.ethereum.enable();
         return true;
     }
